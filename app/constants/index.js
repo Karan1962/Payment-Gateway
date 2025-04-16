2aacb48b1274b5a22da090291dafb4e0cdfceefd
karan <karansawan5050@gmail.com>
2025-04-17 00:41:43 +0530

    dummy pdf portal

---
+++ app/constants/index.js
@@ -0,0 +1,42 @@
+export const invoiceData = [
+  {
+    id: 1,
+    url: "https://drive.google.com/file/d/1-CaoudcJWhbZQcazATj4M1v2DHA57dp6/view?usp=sharing",
+  },
+  {
+    id: 2,
+    url: "https://drive.google.com/file/d/1BN0AGR-eE5qCpo2wkdXM1Zw7iVoR9e4z/view?usp=sharing",
+  },
+  {
+    id: 3,
+    url: "https://drive.google.com/file/d/1FInDrjkDXoKOb49lSu-o0MWq2m7JdlCM/view?usp=sharing",
+  },
+  {
+    id: 4,
+    url: "https://drive.google.com/file/d/1GgUIZlR5KebFFNacS0rXj2nZBHLjRMTM/view?usp=sharing",
+  },
+  {
+    id: 5,
+    url: "https://drive.google.com/file/d/1N-pLwqwaUzNKb1_aTpdSGUWorvZphKO6/view?usp=sharing",
+  },
+  {
+    id: 6,
+    url: "https://drive.google.com/file/d/1VMQoZ1X3tty2EuWpZ_l8DiEi8b3KUB-6/view?usp=sharing",
+  },
+  {
+    id: 7,
+    url: "https://drive.google.com/file/d/1_MdnqyKu13DUOt4LKwt_vz7WroNgyYOU/view?usp=sharing",
+  },
+  {
+    id: 8,
+    url: "https://drive.google.com/file/d/1ib_Rbwdt30v3ju8MoxjfX-kgkB0C1oAA/view?usp=sharing",
+  },
+  {
+    id: 9,
+    url: "https://drive.google.com/file/d/1lwPk0s2mDqJLE1uhWBw10aX0FlE4UlxE/view?usp=sharing",
+  },
+  {
+    id: 10,
+    url: "https://drive.google.com/file/d/1o3rWW_O6_QfFxdK7u4Yi3RfvGdyn6J_y/view?usp=sharing",
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,24 @@
 "use client";
-import Script from "next/script";
-import { useState } from "react";
-declare global {
-  interface Window {
-    Razorpay: any;
-  }
-}
-const PaymentPage = () => {
-  const AMOUNT = 10;
-  const [isProcessing, setIsProcessing] = useState(false);
+import React from "react";
+import { invoiceData } from "./constants/index.js";
+import Link from "next/link";
 
-  const handlePayment = async () => {
-    setIsProcessing(true);
-
-    try {
-      const response = await fetch("/api/create-order", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ amount: AMOUNT, currency: "INR" }),
-      });
-      const data = await response.json();
-      const options = {
-        key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-        amount: AMOUNT * 100,
-        currency: "INR",
-        name: "Karan",
-        description: "Test Transation",
-        order_id: data.orderId,
-        handler: function (response: any) {
-          console.log("payment successful", response);
-        },
-        prefill: {
-          name: "john Doe",
-          email: "johndoe@example.com",
-          contact: "9999999999",
-        },
-        theme: {
-          color: "#3399cc",
-        },
-      };
-      const rzp1 = new window.Razorpay(options);
-      rzp1.open();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsProcessing(false);
-    }
-  };
+const page = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-black ">
-      <Script src="https://checkout.razorpay.com/v1/checkout.js" />
-      <div className="p-6 bg-white dark:bg-slate-800 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Payment Page</h1>
-        <p className="mb-4">Amount to pay : {AMOUNT} INR</p>
-        <button
-          onClick={handlePayment}
-          disabled={isProcessing}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:bg-gray-400"
-        >
-          {isProcessing ? "processing..." : "Pay Now"}
-        </button>
-      </div>
+    <div>
+      <h1>Sale Invoices</h1>
+      {invoiceData.map((item) => {
+        return (
+          <div key={item.id} style={{ paddingBottom: "10px" }}>
+            <p>Invoice No {item.id}</p>
+            <Link href={item.url} target="blank" style={{ color: "blue" }}>
+              view/download
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-export default PaymentPage;
+export default page;
